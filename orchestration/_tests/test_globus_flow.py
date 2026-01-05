6c8f91d0cab9e93738a73d292acd05698d3bee39
David Abramov <dabramov@lbl.gov>
2026-01-05 14:34:13 -0800

    Issue 87: ALCF Recon Flow (#90)
    Simple fixes to the ALCF recon flow as outlined in this issue:
    https://github.com/als-computing/splash_flows/issues/87
    
    Update: also adding a timeout to globus compute jobs to address this
    issue: https://github.com/als-computing/splash_flows/issues/45

--- orchestration/_tests/test_globus_flow.py
+++ orchestration/_tests/test_globus_flow.py
@@ -325,9 +325,9 @@ def test_alcf_recon_flow(mocker: MockFixture):
 
     mock_hpc_reconstruct.assert_called_once()
     mock_hpc_multires.assert_called_once()
-    # HPC is done, so there's 1 successful transfer (data832->alcf).
-    # We have not transferred tiff or zarr => total 1 copy
-    assert mock_transfer_controller.copy.call_count == 1
+    # HPC is done, so there's 2 successful transfer (data832->alcf).
+    # We have not transferred tiff or zarr => total 2 copies
+    assert mock_transfer_controller.copy.call_count == 2
     mock_schedule_pruning.assert_not_called()
 
     # Reset
